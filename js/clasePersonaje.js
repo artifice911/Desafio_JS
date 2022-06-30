98d210437c2696382ba25f167b711c135c847c0d
Sebastian Carro <88806181+artifice911@users.noreply.github.com>
2022-06-30 17:01:15 -0300

    avance

--- js/clasePersonaje.js
+++ js/clasePersonaje.js
@@ -1,12 +1,11 @@
 class Character{
-    constructor(nombre,edad,altura,peso,dinero,raza,genero,){
+    constructor(nombre,edad,altura,peso,dinero,raza,){
         this.nombre = nombre;
         this.edad = edad;
         this.altura = altura;
         this.peso = peso;
         this.dinero = dinero;
         this.raza = raza;
-        this.genero = genero;
 
         this.vida = 100;
         this.experiencia = 0;
@@ -28,19 +27,79 @@ class Character{
     saludar(){
         alert("Hola!, mi nombre es "+this.nombre+" y voy a acompañarte en tu camino.");
     }
+
     darDiagnostico(){
         alert("Tengo %"+this.vida+" de salud.");
     }
+
     revisarBolsillos(){
         alert("Tengo $"+this.dinero+" para comprar equipamiento.");
     }
 }
+let nombre;
+let char;
+let charOk;
+let dataConsola;
+const avatars = ["BigDady","Thimoty","Morgan","Phillips"];
+const Personaje = new Character("Nombre",28,1.79,92,1000,"Human","M");
 
-const char1 = new Character("Timothy",28,1.79,92,1000,"Human","M");
-const char2 = new Character("Norman",28,1.79,92,1000,"Human","M");
-const char3 = new Character("Phillips",28,1.79,92,1000,"Human","M");
 
-char1.saludar();
-char1.darDiagnostico();
-char1.revisarBolsillos();
-prompt("mostrame esta");
+
+pedirNombre();
+
+function pedirNombre(){
+nombre = prompt("Hola forastero! Te encuentras en el yermo hostil de las tierras de Singular. ¿Cómo te llamas?");
+while (nombre=="" || nombre==null){
+    nombre = prompt("Creo que no entendí tu nombre, ¿cómo te llamas?");
+}
+elegirPersonaje();
+}
+
+function elegirPersonaje(){
+    char = parseInt(prompt(nombre+", para comenzar deberás elegir a uno de los siguientes personajes. Elige con un número al personaje para ver su descripcion.\n \n                  1. TIMOTHY          2. MORGAN         3. PHILLIPS \n"));
+    while(char==null || char<=0 || char>=4 || char.length){
+        char = parseInt(prompt("Deberás elegir un personaje para continuar. \n Elige un numero de personaje."));
+    }
+    switch (char) {
+        case 1:
+            charok = confirm("Has elegido a Timothy! \n Lider de exploracion y mercader experto. \n ¿Aceptas este personaje?");
+            if(charok==true){
+                mostrarConsola(char);
+            }
+            else{
+                elegirPersonaje();
+            }
+            break;
+        case 2:
+            charok = confirm("Has elegido a Morgan! \n La cazadora mas fuerte de las tierras altas. \n ¿Aceptas este personaje?");
+            if(charok==true){
+                mostrarConsola(char);
+            }
+            else{
+                elegirPersonaje();
+            }
+            break;
+        case 3:
+            charok = confirm("Has elegido a Phillips! \n Especialista en trampas y francotirador. \n ¿Aceptas este personaje?");
+            if(charok==true){
+                mostrarConsola(char);
+            }
+            else{
+                elegirPersonaje();
+            }
+            break;
+        default:
+            elegirPersonaje();
+            break;
+    }
+}
+
+
+function mostrarConsola(character){
+    Personaje.nombre = avatars[character];
+    dataConsola = parseInt(prompt("       //////////////////////// EDITA TU PERSONAJE ///////////////////////// \n                          " +Personaje.nombre+"  ||  "+Personaje.edad+" años"+"  ||  "+"$"+Personaje.dinero+"  ||  "+Personaje.skillPuntos+" Puntos \n \n 1.VER PERSONAJE \n 2.EDITAR SKILLS \n 3.VER EQUIPAMIENTO \n 4.COMENZAR DESAFÍO \n(compra equipamiento para tu aventura y agrega Skills para poder comenzar el desafío)"));
+}
+
+
+
+
