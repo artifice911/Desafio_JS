07c6e9513e5db9aabc8c4d332e73391482dca4ff
Artifice911 <88806181+artifice911@users.noreply.github.com>
2022-07-31 19:49:18 -0300

    cambios IMGs PATH LISTOS

--- js/nuevoCodigoDOM.js
+++ js/nuevoCodigoDOM.js
@@ -67,7 +67,7 @@ class Equipamiento {
 const Armor1 = new Equipamiento("uno", "ESCUDO", 300, 10, 20, 100, "armor","img/escudo.svg");
 const Armor2 = new Equipamiento("dos", "YELMO", 200, 15, 20, 70, "armor","img/yelmo.svg");
 const Armor3 = new Equipamiento("tres", "PECHERA", 250, 5, 25, 80, "armor","img/pechera.svg");
-const Weapon1 = new Equipamiento("cuatro", "ESPADA", 400, 90, 15, 50, "weapon","/img/espada.svg");
+const Weapon1 = new Equipamiento("cuatro", "ESPADA", 400, 90, 15, 50, "weapon","img/espada.svg");
 const Weapon2 = new Equipamiento("cinco", "ARCO", 350, 45, 10, 30, "weapon","img/arco.svg");
 const Weapon3 = new Equipamiento("seis", "BALLESTA", 300, 70, 20, 10, "weapon","img/ballesta.svg");
 
